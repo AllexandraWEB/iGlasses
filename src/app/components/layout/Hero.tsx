60f81feaecafb644dc0ccf10ef8f32a7b253d388
Alexandra <cimentarowa@gmail.com>
2025-03-24 22:25:26 +0200

    Update

--- src/app/components/layout/Hero.tsx
+++ src/app/components/layout/Hero.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section id='home' className='w-full flex xl:flex-row flex-col justify-center min-h-screen'>
-      <div className="bg-[url('/Hero-Section.svg')] bg-cover bg-center h-screen relative">
-        {/* Logo Container */}
-        <div className='absolute top-0 left-0 w-full h-[231px] mt-[40px] flex flex-wrap items-start border-2'>
-          <img src="Logo.svg" alt="iGlasses Logo" className='ml-[28px]'/>
+    <section id='home' className='w-full flex flex-col md:flex-row xl:flex-row justify-center min-h-screen bg-[url("/Hero-Section.svg")] bg-cover bg-center relative border-2 border-red-500'>
+      
+        <div className='w-full h-[231px] mt-5 flex flex-wrap items-start border-2 border-blue-500'>
+
+          <img src="Logo.svg" alt="iGlasses Logo" className='ml-[20px] mr-4'/>
           <img src="Logo-Decoration.svg" alt="Decoration Logo" className='ml-[77px] mr-[20px]'/>
-          <h1 className='text-[92px] text-primary-grey tracking-[-4px] leading-[78px] uppercase font-inter font-extrabold'>Creating the <br /> future of <br /> vision</h1>
+          <h1 className='text-[42px] md:text-[92px] text-white tracking-[-4px] leading-[78px] uppercase font-inter font-extrabold'>Creating the <br /> future of <br /> vision</h1>
+
         </div>
-      </div>  
+
     </section>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "./components/layout/Hero";
 
 
+
 export default function Home() {
   return (
     <>
