a19f8a2f2df08d79dabc12dbbd6e5a26f779032b
Alexandra <cimentarowa@gmail.com>
2025-03-26 21:44:31 +0200

    Updating the Nav

--- src/app/components/layout/Hero.tsx
+++ src/app/components/layout/Hero.tsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import Nav from './Nav'
-import CartButton from '../ui/CartButton'
 
 const Hero = () => {
   return (
-    <section id='home' className='w-full flex flex-col md:flex-row xl:flex-row min-h-screen bg-[url("/Hero-Section.svg")] bg-cover bg-center relative border-2 border-red-500'>
-      
-        <div className='absolute w-full h-[231px] mt-[40px] flex flex-wrap flex-auto items-start border-2 border-blue-500'>
-
-            <img src="Logo.svg" alt="iGlasses Logo" className='ml-[28px]'/>
-            <img src="Logo-Decoration.svg" alt="Decoration Logo" className='hidden lg:block ml-[45px] md:ml-[77px] ld:ml-[77px] mr-[20px]'/>
-            <h1 className='text-[50px] md:text-[92px] text-white tracking-[-4px] leading-[43px] md:leading-[78px] uppercase font-inter font-extrabold'>Creating the <br /> future of <br /> vision</h1>
-            <Nav /> 
-            <CartButton />
-
+    <section id='home' className='w-full flex flex-col min-h-screen bg-[url("/Hero-Section.svg")] bg-cover bg-center relative border-2 border-red-500'>
+          <div className='w-full h-[250px] border-2 border-blue-600'>
+             <Nav /> 
           </div>
-
+          <div className='w-full mt-[20%] border-2 border-amber-400'> 
+             <h1 className='text-[50px] md:text-[92px] pl-[28px] text-center md:text-start m-auto md:m-0 text-white tracking-[-4px] leading-[43px] md:leading-[78px] uppercase font-inter font-extrabold'>Creating the <br /> future of <br /> vision</h1>
+          </div>
+       
     </section>
   )
 }
@@ -23,3 +18,20 @@ const Hero = () => {
 export default Hero
 
 // Need to fix the h1 (adding padding in the phone version, if needed to be centered)
+
+// <section id='home' className='w-full flex flex-col md:flex-row xl:flex-row min-h-screen bg-[url("/Hero-Section.svg")] bg-cover bg-center relative border-2 border-red-500'>
+        
+// <div className='absolute w-full h-[231px] mt-[40px] flex flex-wrap flex-auto items-start border-2 border-blue-500'>
+
+//     <img src="Logo.svg" alt="iGlasses Logo" className='ml-[28px]'/>
+//     <img src="Logo-Decoration.svg" alt="Decoration Logo" className='hidden lg:block ml-[45px] md:ml-[77px] ld:ml-[77px] mr-[20px]'/>
+//     <div>
+//     <h1 className='text-[50px] md:text-[92px] text-center md:text-start m-auto md:m-0 text-white tracking-[-4px] leading-[43px] md:leading-[78px] uppercase font-inter font-extrabold'>Creating the <br /> future of <br /> vision</h1>
+//     </div>
+//     <div className=''>
+//     <Nav /> 
+//     </div>
+//     {/* <CartButton /> */}
+//  </div>
+
+// </section>
--- src/app/components/layout/Nav.tsx
+++ src/app/components/layout/Nav.tsx
@@ -1,16 +1,92 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react';
+import CartButton from '../ui/CartButton';
 
 const Nav = () => {
-  return (
-    <ul className='flex flex-col justify-start items-start px-[50px] gap-2 max-lg:hidden cursor-pointer text-[14px] uppercase'>
-       <li className="font-bold hover:underline hover:underline-offset-8"><a href="/">Products</a></li>
-       <li className="font-bold hover:underline hover:underline-offset-8"><a href="">About</a></li>
-       <li className="font-bold hover:underline hover:underline-offset-8"><a href="">Events</a></li>
-       <li className="font-bold hover:underline hover:underline-offset-8"><a href="">Press</a></li>
-       <li className="font-bold hover:underline hover:underline-offset-8"><a href="">Contact</a></li>
-    </ul>
-    // Need to add the hamburger icon and phone functionality
-  )
-}
+  const [showMenu, setShowMenu] = useState(false);
 
-export default Nav
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
+    return (
+    <header className='relative padding-x py-8 z-10 w-full bg-light-gray'>
+      <nav className='flex justify-between items-start regular-container'>
+                  <div className="">
+                   <img 
+                   src='Logo.svg' 
+                   alt='Logo' 
+                   className="ml-[30px] md:ml-[28px]"/>
+                  </div>
+                  <div>
+                  <img 
+                  src="Logo-Decoration.svg" 
+                  alt="Decoration Logo" 
+                  className='hidden lg:block ml-[45px] md:ml-[77px] ld:ml-[77px] mr-[20px]'/>
+                  </div>
+            
+              {showMenu && (
+          <div className="md:hidden fixed top-0 left-0 w-full h-screen overscroll-none bg-white z-50 flex justify-center items-center">
+            <ul className="p-4 flex flex-col items-center space-y-10 text-[16px] text-nav-color">
+              <li>
+                <button className="px-4 py-2" onClick={closeMenu}>
+                {showMenu ? (
+                              <img src="/close.svg" alt="Hamburger" width={25} height={25} className=''/>
+                            ) : (
+                              <img src="/hamburger.svg" alt="Hamburger" width={25} height={25} className=''/>
+                            )}
+                </button>
+              </li>
+              <li>
+                <a href='/' onClick={toggleMenu}>Product</a>
+              </li>
+              <li className='mb-4 hover:underline-offset-8'>
+                <a href='/pricing' onClick={toggleMenu}>Pricing</a>
+              </li>
+              <li className='mb-4 hover:underline-offset-8'>
+                <a href='/company' onClick={toggleMenu}>Company</a>
+              </li>
+              <li className='mb-4 hover:underline-offset-8'>
+                <a href='/blog' onClick={toggleMenu}>Blog</a>
+              </li>
+              <li className='mb-4 hover:underline-offset-8'>
+                <a href='/contact' onClick={toggleMenu}>Contact</a>
+              </li>
+              <li className='mb-4 hover:underline-offset-8'>
+                {/* <BlueButton /> */}
+              </li>
+              <li className=''>
+                {/* <WhiteButton /> */}
+              </li>
+            </ul>
+          </div>
+        )}
+         <ul className='flex-1 flex justify-start items-center px-9 gap-6 max-lg:hidden text-nav-color cursor-pointer text-[13px] uppercase font-bold'>
+              <li className="px-5 ml-[180px] hover:underline hover:underline-offset-8"><a href="/">Product</a></li>
+              <li className="px-5 hover:underline hover:underline-offset-8"><a href="/pricing">Pricing</a></li>
+              <li className="px-5 hover:underline hover:underline-offset-8"><a href="/company"> Company</a></li>
+              <li className="px-5 hover:underline hover:underline-offset-8"><a href="/blog">Blog</a></li>
+              <li className="px-5 hover:underline hover:underline-offset-8"><a href="/contact">Contact</a></li>
+          </ul>
+  
+          <ul className='flex justify-end items-center gap-5 max-lg:hidden text-nav-color text-[16px] mr-0 md:mr-[90px]'> 
+              <CartButton />
+              <div className='w-[10px]'>
+              {/* <BlueButton /> */}
+              </div>
+          </ul>
+          <div className='hidden max-lg:block'> 
+            <button onClick={toggleMenu}>
+              <img src="/hamburger.svg" alt="Hamburger" width={25} height={25} className='mr-5'/>            
+           </button>
+          </div>
+      </nav>
+  </header>
+    )
+  }
+  
+  export default Nav
